ee9453223466e27baa4b8207ce9ce4cc4ee741f7
= <=>
2024-02-28 12:14:38 +0600

    api base url changed

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -1,4 +1,5 @@
-String BASE_URL = "http://94.100.26.115:1337/api/";
+// String BASE_URL = "http://94.100.26.115:1337/api/";
+String BASE_URL = "https://noticeapi.techoverltd.com/api/";
 String AUTH_LOGIN = "auth/local";
 String CATEGORIES = "categories";
 String NOTICES = "detailed-notices";
