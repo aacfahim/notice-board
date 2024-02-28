3d5caafed2ccafda0c2e2aba39bd5b832c5c607f
aacfahim <aacfahim@gmail.com>
2024-02-28 15:27:59 +0600

    base url changed

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -1,4 +1,5 @@
 // String BASE_URL = "http://94.100.26.115:1337/api/";
+/// API URLS
 String BASE_URL = "https://noticeapi.techoverltd.com/api/";
 String AUTH_LOGIN = "auth/local";
 String CATEGORIES = "categories";
