7432ab1c595cc21fdbc1314e4029211e6ac3c562
aacfahim <aacfahim@gmail.com>
2024-03-04 17:43:49 +0600

    degree length url

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -10,9 +10,35 @@ String GET_CATEGORIZED_NOTICES =
 String PREFERRED_DETAILED_NOTICE =
     "https://noticeapi.techoverltd.com/v1/api/preferred-detailed-notice";
 
+String DEGREE_LENGTH =
+    "degree-lengths?pagination%5BpageSize%5D=1000&populate=%2A";
+
 
 // All Notice : 1
 // Exam Notice : 2
 // Special Notice : 3
 // Leave Notice : 4
 // Result Notice: 6
+
+
+/// duration_years 
+/// duration_semesters
+/// degree -> degree_name
+/// faculty -> faculty_name
+/// subject -> subject_name
+/// 
+
+/// STRUCTURE 
+
+//data [
+  //attributes{
+/// duration_years,  duration_semesters
+/// degree -> data -> attributes -> degree_name
+/// faculty -> data -> attributes -> faculty_name
+/// subject -> data -> attributes -> subject_name
+/// 
+/// }
+/// ]
+
+
+
