3e6aa29b2c11ab8b24261774ac08c68619c3c7a1
aacfahim <aacfahim@gmail.com>
2024-03-04 17:45:11 +0600

    fetchPreferredNotice api impl

--- lib/features/home/repos/notice_services.dart
+++ lib/features/home/repos/notice_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:notice_board/features/home/model/notice_tile_model.dart';
+import 'package:notice_board/features/home/model/preferred_notice_model.dart';
 import 'package:notice_board/features/notice_detail/bloc/notice_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -11,12 +12,12 @@ import '../../../utils/api_url.dart';
 class NoticeServices {
   static Future<List<NoticeDataModel>> fetchNoticeTile() async {
     NoticeBloc noticeBloc = NoticeBloc();
-    String categoryUrl = BASE_URL + NOTICES;
+    String noticeUrl = BASE_URL + NOTICES;
     var client = http.Client();
     List<NoticeDataModel> notices = [];
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
-      var response = await client.get(Uri.parse(categoryUrl), headers: {
+      var response = await client.get(Uri.parse(noticeUrl), headers: {
         HttpHeaders.contentTypeHeader: "application/json",
         HttpHeaders.authorizationHeader: "Bearer ${prefs.getString("token")}"
       });
@@ -26,6 +27,44 @@ class NoticeServices {
 
         for (var i in data['data']) {
           notices.add(NoticeDataModel.fromJson(i));
+        }
+
+        print("notice tile data: " + jsonEncode(data));
+
+        return notices;
+      } else {
+        print("notice tile data: " +
+            response.statusCode.toString() +
+            " " +
+            response.body);
+      }
+    } catch (e) {
+      print("exception during notice fetch: " + e.toString());
+      noticeBloc.add(HomeNoticeErrorEvent());
+    } finally {
+      client.close();
+    }
+
+    return [];
+  }
+
+  static Future<List<PreferredNoticeModel>> fetchPreferredNotice() async {
+    NoticeBloc noticeBloc = NoticeBloc();
+    String noticeUrl = PREFERRED_DETAILED_NOTICE;
+    var client = http.Client();
+    List<PreferredNoticeModel> notices = [];
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var response = await client.get(Uri.parse(noticeUrl), headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+        HttpHeaders.authorizationHeader: "Bearer ${prefs.getString("token")}"
+      });
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        var data = jsonDecode(response.body);
+
+        for (var i in data) {
+          notices.add(PreferredNoticeModel.fromJson(i));
         }
 
         print("notice tile data: " + jsonEncode(data));
