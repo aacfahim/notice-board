1d84fbf5b70a71bf3e60816c05745e1a59dcf3c4
aacfahim <aacfahim@gmail.com>
2024-03-10 00:47:14 +0600

    preference set api impl

--- lib/features/home/repos/preferred_degree_dropdown.dart
+++ lib/features/home/repos/preferred_degree_dropdown.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:notice_board/features/auth/repos/auth_repo.dart';
 import 'package:notice_board/features/home/model/degree_model.dart';
+import 'package:notice_board/features/home/model/preference_notifier.dart';
 import 'package:notice_board/utils/api_url.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -43,5 +45,57 @@ class PreferredDegree {
     }
 
     return [];
+  }
+
+  static Future setPreference(PreferenceModel preferenceModel) async {
+    String prefUrl = BASE_URL + USER_PREFERENCE;
+    AuthRepo authRepo = AuthRepo();
+    Future<String?> deviceId = authRepo.getDeviceId();
+
+    var client = http.Client();
+
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      Map<String, dynamic> body = {
+        "data": {
+          "year": preferenceModel.selectedYear ?? 0,
+          "semester": preferenceModel.selectedSemester ?? 0,
+          "device_id": deviceId.toString(),
+          "degree": preferenceModel.selectedDegree ?? "null",
+          "faculty": preferenceModel.selectedFaculty ?? "null",
+          "subject": preferenceModel.selectedSubject ?? "null"
+          // "notice_targets": [
+          //   "string or id",
+          //   "string or id"
+          // ]
+        }
+      };
+
+      var response = await client.post(Uri.parse(prefUrl),
+          headers: {
+            HttpHeaders.contentTypeHeader: "application/json",
+            HttpHeaders.authorizationHeader:
+                "Bearer ${prefs.getString("token")}"
+          },
+          body: jsonEncode(body));
+
+      print("res " + response.body);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        var data = jsonDecode(response.body);
+
+        print("pref data: " + jsonEncode(data));
+      } else {
+        print("pred data: " +
+            response.statusCode.toString() +
+            " " +
+            response.body);
+      }
+    } catch (e) {
+      print("exception during pref fetch: " + e.toString());
+    } finally {
+      client.close();
+    }
   }
 }
