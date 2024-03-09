2bf07d6163751164887d27d81335c11048e4843a
aacfahim <aacfahim@gmail.com>
2024-03-10 00:46:51 +0600

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notice_board/features/auth/bloc/auth_bloc.dart';
 import 'package:notice_board/features/auth/repos/auth_repo.dart';
+import 'package:notice_board/features/home/model/preference_notifier.dart';
 import 'package:notice_board/features/home/repos/preferred_degree_dropdown.dart';
 import 'package:notice_board/features/home/ui/custom_navbar.dart';
 import 'package:notice_board/features/home/ui/home.dart';
 import 'package:notice_board/features/notice_detail/ui/notice_detail_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
@@ -16,6 +19,7 @@ void main() {
   // PreferredDegree.fetchDegreeDropdown();
 
   // AuthRepo().getDeviceId();
+
   runApp(const MyApp());
 }
 
@@ -25,19 +29,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notice Board App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff246BFD)),
-        primaryColor: Color(0xff246BFD),
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.rubikTextTheme(
-          Theme.of(context).textTheme,
+    return ChangeNotifierProvider(
+      create: (context) => PreferenceModel(),
+      child: MaterialApp(
+        title: 'Notice Board App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff246BFD)),
+          primaryColor: Color(0xff246BFD),
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.rubikTextTheme(
+            Theme.of(context).textTheme,
+          ),
         ),
+        debugShowCheckedModeBanner: false,
+        home: CustomBottomNavigationBar(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: CustomBottomNavigationBar(),
     );
   }
 }
