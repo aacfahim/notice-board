9fad36d9b1df0688f6109d6ef04545028f91e85c
aacfahim <aacfahim@gmail.com>
2024-03-10 23:03:13 +0600

    splash screen changes

--- lib/features/common/ui/splash_screen.dart
+++ lib/features/common/ui/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:notice_board/utils/const.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,10 +8,21 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: Container(
+      //     decoration: BoxDecoration(
+      //   image: DecorationImage(image: AssetImage("assets/NU_logo.png")),
+      // )),
       body: Container(
-          decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage("assets/NU_logo.png")),
-      )),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("National University Notice Application",
+                  style: TextStyle(color: PRIMARY_COLOR)),
+              SizedBox(height: 10),
+              CircularProgressIndicator(),
+            ],
+          )),
     );
   }
 }
